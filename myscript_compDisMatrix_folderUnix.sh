66527432eec3aec96bb4a17f2266247815a4ca70
Yunsheng Dong <krax@hotmail.it>
2026-03-11 16:13:08 +0100

    add folderunix.sh

---
+++ myscript_compDisMatrix_folderUnix.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# FOLDER="results/subsample_10_1000_16S_fam"
+
+# for file in "$FOLDER"/*.rds; do
+#   name=$(basename "$file" .rds)
+
+#   echo "Running analysis for $name"
+
+#   Rscript compDisMatrix.R -f "$file" -n 3
+
+# done
+#!/bin/bash
+
+FOLDER="results/prova"
+MAX_JOBS=3
+
+mkdir -p "$FOLDER/logs"
+
+find "$FOLDER" -name "*.rds" -print0 | \
+xargs -0 -n 1 -P "$MAX_JOBS" -I {} bash -c '
+file="$1"
+name=$(basename "$file" .rds)
+echo "Running analysis for $name"
+Rscript compDisMatrix.R -f "$file" -n 3 > "'"$FOLDER"'/logs/${name}.log" 2>&1
+' _ {}
+
+echo "done"
